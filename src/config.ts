ce2c76be6f7a2efe06d858d07e4456a010ee6a44
asuka-su <drizzle_0505@outlook.com>
2026-04-12 16:23:21 +0800

    homepage init

---
+++ src/config.ts
@@ -0,0 +1,45 @@
+export const siteConfig = {
+  name: "Chenyu Niu",
+  title: "Senior Student at Peking University",
+  description: "Portfolio website of Chenyu Niu",
+  accentColor: "#1d4ed8",
+  social: {
+    email: "2200017838@stu.pku.edu.cn",
+    github: "https://github.com/asuka-su",
+    bangumi: "https://bgm.tv/user/894993", 
+  },
+  aboutMe:
+    "I'm Chenyu Niu, currently a senior student majoring computer science and technology at Yuanpei College, Peking University. It's my fortune to work as a research intern with Spatial and Temporal Restoration, Understanding and Compression Team (STRUCT) of Wangxuan Institute of Computer Technology. My current research interest lies in image generation and restoration.",
+  projects: [
+    {
+      name: "[ICME 2026] SPDiff: Enhancing Diffusion Models for Scalable Periodic Image Generation",
+      description:
+        "Generate scalable periodic image with Diffusion models!",
+      link: "https://asuka-su.github.io/SPDiff",
+    },
+  ],
+  // experience: [
+  //   {
+  //     company: "Tech Company",
+  //     title: "Senior Software Engineer",
+  //     dateRange: "Jan 2022 - Present",
+  //     bullets: [
+  //       "Led development of microservices architecture serving 1M+ users",
+  //       "Reduced API response times by 40% through optimization",
+  //       "Mentored team of 5 junior developers",
+  //     ],
+  //   },
+  // ],
+  // education: [
+  //   {
+  //     school: "University Name",
+  //     degree: "Bachelor of Science in Computer Science",
+  //     dateRange: "2014 - 2018",
+  //     achievements: [
+  //       "Graduated Magna Cum Laude with 3.8 GPA",
+  //       "Dean's List all semesters",
+  //       "President of Computer Science Club",
+  //     ],
+  //   },
+  // ],
+};
